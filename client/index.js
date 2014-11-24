360ddb8df390782107b9c1ab420533e6b1bbba4b
Zoe Ames <zoeames@gmail.com>
2014-11-24 15:30:30 -0600

    avatar added

---
+++ client/index.js
@@ -0,0 +1,27 @@
+(function(){
+  'use strict';
+
+  angular.module('hapi-auth', ['ui.router', 'LocalForageModule'])
+    .config(['$stateProvider', '$urlRouterProvider', '$localForageProvider', function($stateProvider, $urlRouterProvider, $localForageProvider){
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+        .state('home',     {url:'/',         templateUrl:'/views/home/home.html'})
+        .state('register', {url:'/register', templateUrl:'/views/users/users.html', controller:'UsersCtrl'})
+        .state('login',    {url:'/login',    templateUrl:'/views/users/users.html', controller:'UsersCtrl'});
+
+      $localForageProvider.config({name:'hapi-auth', storeName:'cache', version:1.0});
+    }])
+    .run(['$rootScope', '$http', function($rootScope, $http){
+      $http.get('/status').then(function(response){
+        $rootScope.rootuser = response.data;
+      }, function(){
+        $rootScope.rootuser = null;
+      });
+
+      window.socket = io.connect('/');
+      window.socket.on('online', function(){
+        $rootScope.$broadcast('online');
+      });
+    }]);
+})();

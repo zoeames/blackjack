a65304db736829efa0ccf3340441883581fb4ad6
Zoe Ames <zoeames@gmail.com>
2014-11-25 10:33:46 -0600

    S3 added

--- server/models/user.js
+++ server/models/user.js
@@ -3,54 +3,58 @@
 var mongoose   = require('mongoose'),
     bcrypt     = require('bcrypt'),
     request    = require('request'),
-    fs         = require('fs'),
     path       = require('path'),
+    AWS        = require('aws-sdk'),
     UserSchema = null,
     User       = null;
 
 UserSchema = new mongoose.Schema({
-  username:  {type: String, required: true, validate: [usernameV, 'username length'], unique: true},
-  password:  {type: String, required: true, validate: [passwordV, 'password length']},
-  avatar:    {type: String, required: true},
-  createdAt: {type: Date,  required: true, default: Date.now}
+    username:  {type: String, required: true, validate: [usernameV, 'username length'], unique: true},
+    password:  {type: String, required: true, validate: [passwordV, 'password length']},
+    avatar:    {type: String, required: true},
+    createdAt: {type: Date,  required: true, default: Date.now}
 });
 
 UserSchema.methods.encrypt = function(){
-  this.password = bcrypt.hashSync(this.password, 10);
+    this.password = bcrypt.hashSync(this.password, 10);
 };
 
-UserSchema.methods.download = function(){
-  var assetDir = __dirname + '/../../assets/' + this._id,
-      ext      = path.extname(this.avatar);
+UserSchema.methods.download = function(cb){
+    var s3   = new AWS.S3(),
+        url  = this.avatar,
+        ext  = path.extname(this.avatar),
+        file = this._id + '.avatar' + ext;
 
-  fs.mkdirSync(assetDir);
+    this.avatar = 'https://s3.amazonaws.com/' + process.env.AWS_BUCKET + '/' + file;
 
-  request(this.avatar).pipe(fs.createWriteStream(assetDir + '/avatar' + ext));
-  this.avatar = '/assets/' + this._id + '/avatar' + ext;
+    request({url: url, encoding: null}, function(err, response, body){
+        var params = {Bucket: process.env.AWS_BUCKET, Key: file, Body: body, ACL: 'public-read'};
+        s3.putObject(params, cb);
+    });
 };
 
 UserSchema.statics.login = function(obj, cb){
-  User.findOne({username: obj.username}, function(err, user){
-    if(!user){
-     return cb();
-    }
+    User.findOne({username: obj.username}, function(err, user){
+        if(!user){
+            return cb();
+        }
 
-    var isGood = bcrypt.compareSync(obj.password, user.password);
+        var isGood = bcrypt.compareSync(obj.password, user.password);
 
-    if(!isGood){
-      return cb();
-    }
+        if(!isGood){
+            return cb();
+        }
 
-    cb(user);
-  });
+        cb(user);
+    });
 };
 
 function usernameV(v){
-  return v.length >= 3 && v.length <= 12;
+    return v.length >= 3 && v.length <= 12;
 }
 
 function passwordV(v){
-  return v.length === 60;
+    return v.length === 60;
 }
 
 User = mongoose.model('User', UserSchema);
